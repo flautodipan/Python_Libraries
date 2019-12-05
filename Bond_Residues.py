462351cc3c4ef98e35fac1e906f0ce1425b430f6
flauto_di_pan <panei.1638817@studenti.uniroma1.it>
2019-12-05 17:07:15 +0100

    First commit

---
+++ Bond_Residues.py
@@ -0,0 +1,10 @@
+#%%
+# Script per capire regione di interazione tra domini RRMs della TDP43 e RNA1 
+
+import BioAlessandria as BA
+import numpy as np
+
+#%%
+#1) importo il pdb
+
+TDP43 = BA.Proteina('4bs2')
