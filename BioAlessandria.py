1dbbc47acb91d6023fe550be0a870e0f2487881a
flauto_di_pan <panei.1638817@studenti.uniroma1.it>
2019-12-07 02:19:05 +0100

    Migliorata analisi di clustering, anche se non chiarissima ancora a livello teorico. Analisi silhouette dà k = 3 come miglior numero di clusters nello spazio essenziale. Realizzate rappresentazioni grafiche

--- BioAlessandria.py
+++ BioAlessandria.py
@@ -18,6 +18,8 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
+from operator import methodcaller
+
 class Protein:
 
     def __init__(self, pdb_filename, chain_id = 'A', from_web = False, model = 1):
@@ -190,6 +192,144 @@ class RNA:
             print('Tutto ok \n Selezionati %s atomi di Fosforo pari al numero di residui -1\n Salvate le coordinate cartesiane di ogni atomo P\n\n' %len(self.P_Coord))
         else:
             raise ValueError("Something went wrong in determining size of RNA\n\n")
+    
+class Eigen_Trj():
+
+    """
+    Classe per analisi degli autovalori e autovettori della traiettoria effettuata in simulazione
+    MD con il Software GROMACS
+    --> indicare il path dove si trovano i file eigenval.xvg e eigenvec.trr (solitamente cartella
+    supeiore a quella di questo script)
+    """
+    def __init__(self, path = '../', fig = False):
+
+        _ , self.EigenValues = Parse_xvg(path+'eigenval.xvg')
+
+        if fig :
+            
+            plt.figure()
+            plt.plot(self.EigenValues, '.')
+            plt.title('Eigenvalues of MD trajetory')
+            plt.xlabel('Eig index (ordered by variance significance)')
+            plt.show()
+
+    def Analyze_Variance_Explained_Ratio(self, percent_sigma):
+
+        """
+        
+        Funzione che salva e stampa gli autovalori che spiegano
+        la varianza sigma richiesta da utente 
+    
+        
+        """
+
+        Norma           = np.sum(self.EigenValues)
+        Sigma_ERatio    = self.EigenValues/Norma
+
+        n               = 0
+        test            = 0.
+        Sigma_Eigens    = []
+
+        while True:
+
+            Sigma_Eigens.append(self.EigenValues[n])
+            test        += Sigma_ERatio[n]         
+            n           += 1
+            if test     > percent_sigma: break
+
+        df              = pd.DataFrame(Sigma_Eigens)
+        print('# Autovalori che spiegano la varianza richiesta = ', n, '\n\n')
+        df
+        print(Sigma_Eigens)
+
+class Cluster_2DAnalysis():
+
+    def __init__(self, xy, clustype = 'KMeans', fig = False):
+        
+        self.clustype   =  clustype
+        self.xy         =  xy
+
+    def Elbow_KMeans(self, kmax):
+
+        """ Funzione che implementa il metodo "Elbow" sull'algoritmo k_means
+
+            calcola e mostra la Within-Cluster Sum of Squared errors 
+            per     differenti valori di k, al fine di scegliere il migliore.
+
+        -----
+
+            Parameters
+
+            points sono i dati su cui clusterizzare
+            variando k tra 1 e kmax
+
+        """
+    
+        self.Elbow = []
+
+        for k in range(1, kmax+1):
+
+            kmeans = KMeans(n_clusters = k).fit(self.xy)
+            centroids = kmeans.cluster_centers_
+            pred_clusters = kmeans.predict(self.xy)
+            curr_sse = 0
+    
+            # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
+            for i in range(len(self.xy)):
+
+                curr_center = centroids[pred_clusters[i]]
+                curr_sse += (self.xy[i, 0] - curr_center[0]) ** 2 + (self.xy[i, 1] - curr_center[1]) ** 2
+      
+            self.Elbow.append(curr_sse)
+        
+
+        plt.figure()
+        plt.plot(range(kmax), self.Elbow)
+        plt.title('Elbow check for K-means')
+        plt.xlabel('k value')
+        plt.savefig('Elbow_test')
+
+    def Silhouette_KMeans(self, kmax):
+
+        self.Silh = []
+
+        # dissimilarity would not be defined for a single cluster, thus, minimum number of clusters should be 2
+        for k in range(2, kmax+1):
+            kmeans = KMeans(n_clusters = k).fit(self.xy)
+            labels = kmeans.labels_
+            self.Silh.append(silhouette_score(self.xy, labels, metric = 'euclidean'))
+
+        self.n_clusters =  np.array(self.Silh).argmax() +2 
+        
+        plt.figure()
+        plt.plot(np.arange(0,kmax-1,1)+2, self.Silh)
+        plt.title('Silohuette check for k-means')
+        plt.xlabel('k value')
+        plt.savefig('Silh_test')
+
+    def Clusterize(self):
+
+        if (self.clustype == 'KMeans'):
+
+            self.Cluster = KMeans(n_clusters=self.n_clusters).fit(self.xy)
+        else:
+            raise ValueError ("Non è stato selezionato un algoritmo di clustering corretto\
+                Scegliere:\n 'KMeans' per algoritmo k-means\n")    
+
+    def Figure_Clusters(self):
+
+        c   =   self.Cluster.labels_
+
+        fig = plt.figure()
+        
+        plt.scatter(self.xy[:,0], self.xy[:,1], c=c, cmap= 'viridis', s = 10)
+        plt.colorbar()
+        plt.scatter(self.Cluster.cluster_centers_[:,0],self.Cluster.cluster_centers_[:,1], marker='*', edgecolors='black')
+        plt.xlabel(' PC 1 ')
+        plt.ylabel(' PC 2 ')
+        plt.title(str(self.n_clusters)+' Cluster representation by '+self.clustype+' algorithm')
+        plt.show()
+        fig.savefig('3 micros/Clusters.png')
 
 def BioStructure (pdb_filename, n_structures, structures_names, structures_type, structures_chain_ids, structures_models):
 
@@ -216,8 +356,7 @@ def BioStructure (pdb_filename, n_structures, structures_names, structures_type,
         exec(command)
 
 
-def Dist_Matrix (Coord):
-    
+def Dist_Matrix (Coord):  
 # prende matrice coordinate x,y,z di N elementi, ritorna matrice delle distanze relative
 # capire se come in C è un problema ritornare una matrice, 
 # è meglio fare con i puntatori
@@ -332,121 +471,7 @@ def Analyze_Bond_Residues (Cont_Matrix, structure_sizes, structure_names):
         RNA_Bonds = RNA_Bonds + (RNA_i,)
     
     return RNA_Bonds
-    
-class Eigen_Trj():
 
-    """
-    Classe per analisi degli autovalori e autovettori della traiettoria effettuata in simulazione
-    MD con il Software GROMACS
-    --> indicare il path dove si trovano i file eigenval.xvg e eigenvec.trr (solitamente cartella
-    supeiore a quella di questo script)
-    """
-    def __init__(self, path = '../', fig = False):
-
-        _ , self.EigenValues = Parse_xvg(path+'eigenval.xvg')
-
-        if fig :
-            
-            plt.figure()
-            plt.plot(self.EigenValues, '.')
-            plt.title('Eigenvalues of MD trajetory')
-            plt.xlabel('Eig index (ordered by variance significance)')
-            plt.show()
-
-    def Analyze_Variance_Explained_Ratio(self, percent_sigma):
-
-        """
-        
-        Funzione che salva e stampa gli autovalori che spiegano
-        la varianza sigma richiesta da utente 
-    
-        
-        """
-
-        Norma           = np.sum(self.EigenValues)
-        Sigma_ERatio    = self.EigenValues/Norma
-
-        n               = 0
-        test            = 0.
-        Sigma_Eigens    = []
-
-        while True:
-
-            Sigma_Eigens.append(self.EigenValues[n])
-            test        += Sigma_ERatio[n]         
-            n           += 1
-            if test     > percent_sigma: break
-
-        df              = pd.DataFrame(Sigma_Eigens)
-        print('# Autovalori che spiegano la varianza richiesta = ', n, '\n\n')
-        df
-        print(Sigma_Eigens)
-
-class Cluster_2DAnalysis():
-
-    def __init__(self, xy, clustype = 'k-means', fig = False):
-        
-        self.clustype   =  clustype
-        self.xy         =  xy
-
-    def Elbow_KMeans(self, kmax):
-
-        """ Funzione che implementa il metodo "Elbow" sull'algoritmo k_means
-
-            calcola e mostra la Within-Cluster Sum of Squared errors 
-            per     differenti valori di k, al fine di scegliere il migliore.
-
-        -----
-
-            Parameters
-
-            points sono i dati su cui clusterizzare
-            variando k tra 1 e kmax
-
-        """
-
-        self.Elbow = []
-
-        for k in range(1, kmax+1):
-
-            kmeans = KMeans(n_clusters = k).fit(self.xy)
-            centroids = kmeans.cluster_centers_
-            pred_clusters = kmeans.predict(self.xy)
-            curr_sse = 0
-    
-            # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
-            for i in range(len(self.xy)):
-
-                curr_center = centroids[pred_clusters[i]]
-                curr_sse += (self.xy[i, 0] - curr_center[0]) ** 2 + (self.xy[i, 1] - curr_center[1]) ** 2
-      
-            self.Elbow.append(curr_sse)
-        
-
-        plt.figure()
-        plt.plot(range(kmax), self.Elbow)
-        plt.title('Elbow check for K-means')
-        plt.xlabel('k value')
-        plt.savefig('Elbow_test')
-
-    def Silhouette_KMeans(self, kmax):
-
-        self.Silh = []
-
-        # dissimilarity would not be defined for a single cluster, thus, minimum number of clusters should be 2
-        for k in range(2, kmax+1):
-            kmeans = KMeans(n_clusters = k).fit(self.xy)
-            labels = kmeans.labels_
-            self.Silh.append(silhouette_score(self.xy, labels, metric = 'euclidean'))
-
-        plt.figure()
-        plt.plot(range(kmax-1), self.Silh)
-        plt.title('Silohuette check for k-means')
-        plt.xlabel('k value')
-        plt.savefig('Silh_test')
-
-
-from operator import methodcaller
 
 def Parse_xvg(filepath):
 
@@ -458,8 +483,3 @@ def Parse_xvg(filepath):
 
         temp    =  np.array([row.split() for row in filter(methodcaller('startswith', ' '), file.readlines())], dtype=np.float64)
         return     temp[:,0],  temp[:,1]
-
-"Sto facendo una modifica a cazzo de merda"
-
-"Ne sto facendo un'altra vivaddio"
-"Mannaggia alla madonna"
---
+++ Cluster_Analyzer.py
@@ -0,0 +1,58 @@
+#%%
+import time
+start = time.process_time()
+from operator import methodcaller
+import BioAlessandria as BA
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot
+from sklearn.cluster import KMeans
+
+
+#%%
+#RNA 2
+EigVal_RNA2 = BA.Eigen_Trj(path = '3 micros/', fig = True)
+EigVal_RNA2.Analyze_Variance_Explained_Ratio(0.65)
+
+# %%
+
+x,y =  BA.Parse_xvg('1micros/2dproj.xvg')
+#per un motivo stupiddo ci sta un frame di troppo
+x   =   x[np.arange(x.size-1)]
+y   =   y[np.arange(y.size-1)]
+#faccio sampling ogni 100 frame
+indices = np.arange(0, x.size, 100)
+x   =   x[indices]
+y   =   y[indices]
+
+
+
+
+fig = plt.figure()
+c = range(x.size)
+plt.scatter(x,y, c=c, cmap= 'viridis', s = 10)
+plt.colorbar()
+plt.xlabel(' PC 1 ')
+plt.ylabel(' PC 2 ')
+plt.title('Projection of MD trajectory in Essential Space')
+plt.show()
+fig.savefig('3 micros/Essential.png')
+
+# %%
+
+xy = np.array([x,y]).T
+
+RNA2_Clust = BA.Cluster_2DAnalysis(xy)
+
+RNA2_Clust.Elbow_KMeans(15)
+#%%
+xy = np.array([x,y]).T
+RNA2_Clust = BA.Cluster_2DAnalysis(xy)
+RNA2_Clust.Silhouette_KMeans(15)
+
+# %%
+
+RNA2_Clust.Clusterize()
+RNA2_Clust.Figure_Clusters()
+
+#%%
