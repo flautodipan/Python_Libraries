270c1ee44270304ba0b6c7a81ca22000546d5e7c
flautodipan <panei.1638817@studenti.uniroma1.it>
2020-03-08 18:19:21 +0100

    Grafichini

--- graphix.py
+++ graphix.py
@@ -52,46 +52,24 @@ print(path)
 # %%
 # MODELLO RILASSAMENTO
 
+path = '../Scrittek/figures/'
+from Models import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+#Spettro Rayleigh Brillouin
+
 
 
 x = np.linspace(-15, 15, 1000)
-y1 = S_Dynamical_Form_Factor_2_nodelta(x, 10., 7.9, 0.2, 0.3, 0.01)
-y2 = S_Dynamical_Form_Factor_2_nodelta(x, 10., 6.5, 0.35, 0.8, 0.1)
-y3 = S_Dynamical_Form_Factor_2_nodelta(x, 10., 1.4, 0.27, 0.1, 10)
-y4 = S_Dynamical_Form_Factor_2_nodelta(x, 10., 8.7, 0.3, 0.9, 1)
+y1 = S_Dynamical_Form_Factor_2(x, 10., 7.9, 0.2, 0.3, 0.01, 0, 8, 0.6)
+plt.figure()
+plt.plot(x, y1)
+plt.title('Example of Rayleigh Brillouin Spectrum')
+plt.xlabel('$\omega$ - frequency shift (GHz)')
+#plt.set_yticklabels([])
+plt.savefig(path+'spectrum_example.pdf', format = 'pdf')
 
-
-fig, axs = plt.subplots(2, 2, constrained_layout=True)
-fig.suptitle('Dynamical form factors in viscoelastic approximation', fontsize = 15)
-
-
-axs[0,0].set_title('$\Omega$ = {}\t$\Gamma$={}\t$\Delta$={}\t \u03C4={}'.format(7.9, 0.2,0.3, 0.1), fontsize = 8.5)
-axs[0,0].set_ylabel('Intensity')
-axs[0, 0].plot(x, y1, ls = 'solid', color = 'darkgoldenrod')
-plt.ylim(-0.5, 4.5)
-
-
-axs[0,1].set_title('$\Omega$ = {}\t$\Gamma$ = {}\t$\Delta$={}\t \u03C4={}'.format(6.5, 0.35, 0.8, 0.1), fontsize = 8.5)
-plt.ylim(-0.5, 4.5)
-axs[0,1].plot(x, y2, ls = 'solid', color = 'red')
-axs[1,0].set_ylabel('Intensity')
-
-axs[1,0].plot(x, y3, ls = 'solid', color = 'cornflowerblue')
-axs[1,0].set_title('$\Omega$ = {}\t$\Gamma$ = {}\t$\Delta$={}\t \u03C4={}'.format(1.4, 0.27, 0.1, 10), fontsize = 8.5)
-plt.ylim(-0.5, 4.5)
-
-axs[1,0].set_xlabel('$\omega$ (GHz)')
-
-axs[1,1].set_title('$\Omega$ = {}\t$\Gamma$ = {}\t$\Delta$={}\t \u03C4={}'.format(8.7, 0.3, 0.9, 10), fontsize = 8.5)
-plt.ylim(-0.5, 4.5)
-
-axs[1,1].plot(x, y4, ls = 'solid', color = 'darkolivegreen', label = '$\Omega$ = {}\n$\Gamma$ = {}'.format(7.4, 0.27))
-plt.xlabel('$\omega$ (GHz)')
-
-#fig.title('Theoretical function by Markovian model')
-#fig.tight_layout()
-plt.show()
-plt.close()
-fig.savefig(path+'Brillouin_real_example.pdf', format = 'pdf')
 
 # %%
